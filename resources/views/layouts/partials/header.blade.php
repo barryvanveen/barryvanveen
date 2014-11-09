77d3bcb45c54a58753671dac246883f42d5edde3
Barry van Veen <barryvanveen@gmail.com>
2014-11-09 10:43:11 +0100

    opruimen

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
     <div class="container">
 
         <!-- Brand and toggle get grouped for better mobile display -->
