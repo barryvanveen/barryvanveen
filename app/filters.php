29f330264bd0738565f1b50783ba78ea3ed7825c
Barry van Veen <barryvanveen@gmail.com>
2014-11-14 13:38:47 +0100

    fix security problem in csrf-filter

--- app/filters.php
+++ app/filters.php
@@ -83,8 +83,8 @@ Route::filter('guest', function()
 
 Route::filter('csrf', function()
 {
-	if (Session::token() != Input::get('_token'))
-	{
-		throw new Illuminate\Session\TokenMismatchException;
-	}
+    if (Session::token() !== Input::get('_token'))
+    {
+        throw new Illuminate\Session\TokenMismatchException;
+    }
 });
