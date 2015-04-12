552c9e32e2bff800a3f69251a0749595b779f379
Barry van Veen <barryvanveen@gmail.com>
2015-04-12 18:05:01 +0200

    rewrite texts for error pages

--- app/views/templates/403.blade.php
+++ app/views/templates/403.blade.php
@@ -2,11 +2,15 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="jumbotron">
-                <h1>Sorry, je hebt geen toegang tot deze pagina.</h1>
-            </div>
+    <div class="page__content">
+        <div class="page-header">
+            <h1>Oh snap!</h1>
         </div>
+
+        <p>Sorry maar je hebt geen toegang tot deze pagina. Als je denkt dat dit niet klopt, stuur me dan <a
+            href="mailto:barryvanveen@gmail.com">een mailtje</a> om te vragen waarom je deze melding ziet.</p>
+
+        <p><a href="{{ route('home') }}">Ga naar de homepage</a>.</p>
     </div>
+
 @stop
--- app/views/templates/500.blade.php
+++ app/views/templates/500.blade.php
@@ -2,11 +2,19 @@
 
 @section('content')
 
-	<div class="row">
-        <div class="col-xs-12">
-            <div class="jumbotron">
-                <h1>Er is iets fout gegaan.</h1>
-            </div>
+    <div class="page__content">
+        <div class="page-header">
+            <h1>Oeps!</h1>
         </div>
-	</div>
+
+        <p>Sorry, er lijkt iets heel erg mis te gaan. Je ziet deze pagina omdat er een onbekende fout is opgetreden.
+            Deze foutmelding wordt opgeslagen en ik ga uitzoeken hoe we dit in de toekomst kunnen voorkomen.</p>
+
+        <p>Als je me wilt helpen kun je me <a href="mailto:barryvanveen@gmail.com">een mailtje sturen</a> en uitleggen
+            hoe je deze foutmelding te zien kreeg. Dan kan ik je ook laten weten wat er fout ging en wanneer het is
+            opgelost.</p>
+
+        <p><a href="{{ route('home') }}">Ga naar de homepage</a>.</p>
+    </div>
+
 @stop
