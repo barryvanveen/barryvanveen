285ce3184d9840f845bcf8a8de286fc779f3d298
Barry van Veen <barryvanveen@gmail.com>
2015-05-05 16:58:43 +0200

    added syntax highlighting with PrismJS

--- gulpfile.js
+++ gulpfile.js
@@ -20,12 +20,33 @@ var config = {
             'bower_components/autosize/dest/autosize.js',
             'bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
             'bower_components/ajaxq/ajaxq.js',
+            'bower_components/prism/prism.custom.min.js',
 			'resources/assets/js/main.js'
 		],
 		ie8: [
 			'bower_components/html5shiv/dist/html5shiv.js',
 			'bower_components/respond/dest/respond.matchmedia.addListener.src.js'
-		]
+		],
+        prism: [
+            // default
+            'bower_components/prism/components/prism-core.js',
+            'bower_components/prism/components/prism-markup.js',
+            'bower_components/prism/components/prism-css.js',
+            'bower_components/prism/components/prism-css-extras.js',
+            'bower_components/prism/components/prism-clike.js',
+            'bower_components/prism/components/prism-javascript.js',
+            // custom
+            'bower_components/prism/components/prism-bash.js',
+            'bower_components/prism/components/prism-git.js',
+            'bower_components/prism/components/prism-markdown.js',
+            'bower_components/prism/components/prism-php.js',
+            'bower_components/prism/components/prism-php-extras.js',
+            'bower_components/prism/components/prism-scss.js',
+            'bower_components/prism/components/prism-sql.js',
+            // plugins
+            'bower_components/prism/plugins/autolinker/prism-autolinker.js',
+            'bower_components/prism/plugins/line-numbers/prism-line-numbers.js'
+        ]
 	}
 };
 
@@ -58,21 +79,29 @@ gulp.task('build-sass', function () {
  * concatenate all js files
  */
 gulp.task('build-js', function () {
-	gulp.src(config.scripts.src)
+	gulp.src(config.scripts.prism)
         .pipe(plumber({
             errorHandler: onError
         }))
 		.pipe(uglify())
-		.pipe(concat('main.min.js'))
-		.pipe(gulp.dest('public_html/js'));
+		.pipe(concat('prism.custom.min.js'))
+		.pipe(gulp.dest('bower_components/prism'));
 
-	gulp.src(config.scripts.ie8)
+    gulp.src(config.scripts.ie8)
         .pipe(plumber({
             errorHandler: onError
         }))
-		.pipe(uglify())
-		.pipe(concat('main.ie8.min.js'))
-		.pipe(gulp.dest('public_html/js'));
+        .pipe(uglify())
+        .pipe(concat('main.ie8.min.js'))
+        .pipe(gulp.dest('public_html/js'));
+
+    gulp.src(config.scripts.src)
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(uglify())
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest('public_html/js'));
 });
 
 /**
@@ -85,6 +114,10 @@ gulp.task('move', function () {
 
     gulp.src('bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css')
         .pipe(rename("bootstrap-datetimepicker.scss"))
+        .pipe(gulp.dest('resources/assets/scss/bower_components'));
+
+    gulp.src('bower_components/prism/themes/prism-okaidia.css')
+        .pipe(rename("prism-okaidia.scss"))
         .pipe(gulp.dest('resources/assets/scss/bower_components'));
 });
 
@@ -105,4 +138,4 @@ gulp.task('watch-js', function(){
 /**
  * perform these tasks when running just 'gulp'
  */
-gulp.task('default', ['build-sass', 'build-js', 'watch-sass', 'watch-js']);
+gulp.task('default', ['move', 'build-sass', 'build-js', 'watch-sass', 'watch-js']);
