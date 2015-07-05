6c46592ffb021fd1c53a8a3057a9a22e27bf366e
Barry van Veen <barryvanveen@gmail.com>
2015-07-05 22:36:37 +0200

    misc things

--- resources/views/form/partials/errors.blade.php
+++ resources/views/form/partials/errors.blade.php
@@ -12,16 +12,3 @@
         </div>
     </div>
 @endif
-
-{{--
-todo: errors controleren
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
---}}
