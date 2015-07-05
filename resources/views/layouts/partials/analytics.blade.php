bca8029c714a944086261417e0b4ae518e53ecc9
Barry van Veen <barryvanveen@gmail.com>
2015-07-05 22:34:25 +0200

    fixed GA code from config

--- resources/views/layouts/partials/analytics.blade.php
+++ resources/views/layouts/partials/analytics.blade.php
@@ -1,5 +1,4 @@
-{{-- todo: check GA code --}}
-@if ($ga_code !== '')
+@if (!empty($ga_code))
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
