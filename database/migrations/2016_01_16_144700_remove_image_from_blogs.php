ba95cc85c165b833a7758cee123b4e7e2498574a
Barry van Veen <barryvanveen@gmail.com>
2016-01-16 16:05:08 +0100

    php-cs-fixer

--- database/migrations/2016_01_16_144700_remove_image_from_blogs.php
+++ database/migrations/2016_01_16_144700_remove_image_from_blogs.php
@@ -1,14 +1,12 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class RemoveImageFromBlogs extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
@@ -19,8 +17,6 @@ class RemoveImageFromBlogs extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
