6bb7a6411857e57393a07eb6a23d98b073295b93
Barry van Veen <barryvanveen@gmail.com>
2016-02-01 15:45:27 +0100

    load webfont asyncronously

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,6 +25,21 @@
 	</head>
 	<body itemscope itemtype="http://schema.org/WebPage">
 
+        <script type="text/javascript">
+            WebFontConfig = {
+                google: { families: [ 'Raleway:400,700:latin' ] }
+            };
+            (function() {
+                var wf = document.createElement('script');
+                wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+                        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+                wf.type = 'text/javascript';
+                wf.async = 'true';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(wf, s);
+            })();
+        </script>
+
 		@include('layouts.partials.analytics')
 
 		@include('layouts.partials.header')
