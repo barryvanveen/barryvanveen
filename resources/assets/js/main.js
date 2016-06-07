fbe60dd3c5cd93ceb70e6f92d2867ea551a5a980
Barry van Veen <barryvanveen@gmail.com>
2016-06-07 21:09:12 +0200

    fixed problem with loading dist/js/gameoflife.js

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -106,8 +106,8 @@ window.Barryvanveen.initGameoflife = function() {
 		return;
 	}
 
-	LazyLoad.js('dist/js/gameoflife.min.js', window.Barryvanveen.startGameoflife);
-
+	LazyLoad.js('dist/js/gameoflife.js', window.Barryvanveen.startGameoflife);
+	
 };
 
 window.Barryvanveen.startGameoflife = function() {
@@ -123,6 +123,45 @@ window.Barryvanveen.startGameoflife = function() {
 	$('.js-gameoflife-step').click(function() {window.Barryvanveen.gameoflife.step();});
 	$('.js-gameoflife-stop').click(function() {window.Barryvanveen.gameoflife.stop();});
 	$('.js-gameoflife-reset').click(function() {window.Barryvanveen.gameoflife.reset();});
+	$('.js-gameoflife-gun').click(function() {window.Barryvanveen.gameoflife.setState([
+			{col: 1, row: 21},
+			{col: 1, row: 22},
+			{col: 2, row: 21},
+			{col: 2, row: 22},
+			{col: 11, row: 21},
+			{col: 11, row: 22},
+			{col: 11, row: 23},
+			{col: 12, row: 20},
+			{col: 12, row: 24},
+			{col: 13, row: 19},
+			{col: 13, row: 25},
+			{col: 14, row: 19},
+			{col: 14, row: 25},
+			{col: 15, row: 22},
+			{col: 16, row: 20},
+			{col: 16, row: 24},
+			{col: 17, row: 21},
+			{col: 17, row: 22},
+			{col: 17, row: 23},
+			{col: 18, row: 22},
+			{col: 21, row: 19},
+			{col: 21, row: 20},
+			{col: 21, row: 21},
+			{col: 22, row: 19},
+			{col: 22, row: 20},
+			{col: 22, row: 21},
+			{col: 23, row: 18},
+			{col: 23, row: 22},
+			{col: 25, row: 17},
+			{col: 25, row: 18},
+			{col: 25, row: 22},
+			{col: 25, row: 23},
+			{col: 35, row: 19},
+			{col: 35, row: 20},
+			{col: 36, row: 19},
+			{col: 36, row: 20}
+		]);
+	});
 
 };
 
