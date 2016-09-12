c19e4b1f34bd75452fbecd9ea492be3fbfcd5b36
Barry van Veen <barryvanveen@gmail.com>
2016-09-12 21:14:22 +0200

    fixed blog rss test

--- tests/functional/BlogTest.php
+++ tests/functional/BlogTest.php
@@ -85,7 +85,6 @@ class BlogTest extends TestCase
 
         $this->visit(route('blog-rss'));
 
-        $this->see('<rss version="2.0"')
-            ->see('<title>'.$blog->title.'</title>');
+        $this->see('<title>'.$blog->title.'</title>');
     }
 }
